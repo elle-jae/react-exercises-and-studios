5a216c6caff82948718167f9930bf0fd223085b7
Ellen Jardon <e@ellens-mbp.lan>
2023-08-03 15:54:39 -0500

    added part 2 of excercie

---
+++ part1/exercises/src/components/HobbyLinks.js
@@ -0,0 +1,10 @@
+
+export default function HobbyLinks() {
+    let hobbyLinks = ["https://www.rei.com", "https://www.climbkc.com"];
+return (
+<div>
+    <a href = {hobbyLinks[0]}>REI</a> <br></br>
+    <a href = {hobbyLinks[1]}>RoKC</a>
+</div>
+    );
+}
